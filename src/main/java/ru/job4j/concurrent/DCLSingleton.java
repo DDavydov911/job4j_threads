392306f1d01448282c589761bedc6e8e58b8c28e
Denis Davydov <dvd9@yandex.ru>
2022-01-24 16:15:25 +0300

    2. Модель памяти Java [#267917]

--- src/main/java/ru/job4j/concurrent/DCLSingleton.java
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -2,17 +2,17 @@ package ru.job4j.concurrent;
 
 public final class DCLSingleton {
 
-    private static DCLSingleton inst;
+    private static volatile DCLSingleton inst; /* Переменная была не синхронизирована. Из-за этого
+     возможна ситуация, при которой в момент создания объекта, другой поток может получить
+     и начать использовать не полностью сконструированный объект.*/
 
     public static DCLSingleton instOf() {
-        /*if (inst == null) {   Это условие может проверяться
-        сразу несколькими потоками, этот код не синхронизирован.
-        Нужен только код следующий ниже.*/
-            synchronized (DCLSingleton.class) { /* А этот код синхронизирует единственный объект класса*/
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
                 if (inst == null) {
                     inst = new DCLSingleton();
                 }
-            /*}*/
+            }
         }
         return inst;
     }
