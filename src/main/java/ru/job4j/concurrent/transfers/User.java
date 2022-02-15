1d27117d24fb5adcae423455076ca9058ac67f75
Denis Davydov <dvd9@yandex.ru>
2022-02-15 10:19:17 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #256000] fixed

--- src/main/java/ru/job4j/concurrent/transfers/User.java
+++ src/main/java/ru/job4j/concurrent/transfers/User.java
@@ -1,13 +1,8 @@
 package ru.job4j.concurrent.transfers;
 
-import net.jcip.annotations.GuardedBy;
-import net.jcip.annotations.ThreadSafe;
-
-@ThreadSafe
 public class User {
     private final int id;
 
-    @GuardedBy("this")
     private int amount;
 
     public User(int id) {
@@ -18,11 +13,11 @@ public class User {
         return id;
     }
 
-    public synchronized int getAmount() {
+    public int getAmount() {
         return amount;
     }
 
-    public synchronized void setAmount(int amount) {
+    public void setAmount(int amount) {
         this.amount = amount;
     }
 }
--- src/main/java/ru/job4j/concurrent/transfers/UserStorage.java
+++ src/main/java/ru/job4j/concurrent/transfers/UserStorage.java
@@ -3,29 +3,31 @@ package ru.job4j.concurrent.transfers;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    private final List<User> list = new ArrayList<>();
+    private final Map<Integer, User> users = new HashMap<>();
 
-    public synchronized boolean add (User user) {
-        return list.add(user);
+    public synchronized boolean add(User user) {
+        User u = users.putIfAbsent(user.getId(), user);
+        return (u == null);
+    }
+
+    public synchronized boolean update(User user) {
+        User old = users.get(user.getId());
+        User u = users.replace(user.getId(), user);
+        return u != null &&  old != u;
     }
 
     public synchronized boolean delete(User user) {
-        return list.remove(user);
+        return users.remove(user.getId(), user);
     }
 
     private synchronized User getUser(int id) {
-        for (User user : list) {
-            if (user.getId() == id) {
-                return user;
-            }
-        }
-        return null;
+        return users.get(id);
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
