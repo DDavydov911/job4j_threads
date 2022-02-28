3e91e7a1b75b2ce3f2f560ad93e6a882a2e7909c
Denis Davydov <dvd9@yandex.ru>
2022-02-28 21:06:55 +0300

    2. Обеспечить остановку потребителя. [#66825 #270270]

--- src/main/java/ru/job4j/concurrent/ParallelSearch.java
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -2,34 +2,32 @@ package ru.job4j.concurrent;
 
 public class ParallelSearch {
     public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
-        final Thread producer = new Thread(
-                () -> {
-                    for (int index = 0; index < 3; index++) {
-                        try {
-                            queue.offer(index);
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                    Thread.currentThread().interrupt();
-                }
-        );
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
         final Thread consumer = new Thread(
                 () -> {
-                    while (!producer.isInterrupted()) {
+                    while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
                     }
-                    Thread.currentThread().interrupt();
+
                 }
         );
-
-        producer.start();
         consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
     }
 }
