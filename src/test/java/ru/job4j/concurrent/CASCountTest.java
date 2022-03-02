7f314db2bb84af616e5352b1055b74fa1c03af49
Denis Davydov <dvd9@yandex.ru>
2022-03-02 20:25:32 +0300

    0. CAS - операции [#6859 #271063] 2Thread

--- src/test/java/ru/job4j/concurrent/CASCountTest.java
+++ src/test/java/ru/job4j/concurrent/CASCountTest.java
@@ -2,6 +2,10 @@ package ru.job4j.concurrent;
 
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -16,5 +20,32 @@ public class CASCountTest {
         assertThat(count.get(), is(2));
         count.increment();
         assertThat(count.get(), is(3));
+    }
+
+    @Test
+    public void when2ThreadIncrement() throws InterruptedException {
+        CASCount count = new CASCount();
+        Set<Integer> set = new TreeSet<>();
+        Thread first = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        count.increment();
+                        set.add(count.get());
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        count.increment();
+                        set.add(count.get());
+                    }
+                }
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertEquals(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), set);
     }
 }
